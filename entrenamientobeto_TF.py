feb70adbf0241b74cb8c24ff6dac4500a02cf101
Jonathan Zavala <60908853+zavaladiazjonathan@users.noreply.github.com>
2024-05-07 14:18:19 -0600

    Update entrenamientobeto_TF.py

--- entrenamientobeto_TF.py
+++ entrenamientobeto_TF.py
@@ -1,7 +1,9 @@
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import time
 
+start_time = time.time()
 # Cargar y preprocesar el conjunto de datos CIFAR-10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -30,3 +32,7 @@ history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
 print('Exactitud en el conjunto de prueba:', test_acc)
+
+execution_time = time.time() - start_time
+
+print(f"Tiempo de ejecución: {execution_time:.2f} segundos")
